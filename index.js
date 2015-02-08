f1b283aafdde03afcd2a3db828798c5aab629e34
ArcherZhao <zlming1015@gmail.com>
2015-02-08 12:15:36 +0800

    change the method

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
 (function (root, factory) {
 	'use strict';
-	
+
 	if (typeof exports !== 'undefined') {
 		module.exports = factory(root);
 	} else {
 		root.validator = factory(root);
 	}
-	
+
 }(this, function (root) {
 	'use strict';
-	
+
 	var previousValidator = root.validator,
 		validator = {};
-	
+
 	validator.VERSION = '0.0.1';
-	
+
 	validator.noConflict = function () {
 		root.validator = previousValidator;
 		return this;
 	};
+
+	validator.extend = function (obj) {
+		var type = typeof obj;
+		if (!(type === 'function' || type === 'object' && !!obj)) return obj;
+		var source, prop;
+		for (var i = 1, length = arguments.length; i < length; i++) {
+			source = arguments[i];
+			for (prop in source) {
+				if (hasOwnProperty.call(source, prop)) {
+					obj[prop] = source[prop];
+				}
+			}
+		}
+		return obj;
+	};
+
+	var method = {
+		isPhone: function () {
+			return /^(?:(?:1(?:3[4-9]|5[012789]|8[78])\d{8}|1(?:3[0-2]|5[56]|8[56])\d{8}|18[0-9]\d{8}|1[35]3\d{8})|14[57]\d{8}|170[059]\d{7}|17[67]\d{8})$/.test(this);
+		},
+		isMail: function () {
+			return /^([_a-zA-Z\d\-\.])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/.test(this);
+		},
+		isFill: function () {
+			return this.length !== 0;
+		},
+		isPassword: function () {
+			return /^[a-zA-Z0-9_-]{6,20}$/.test(this);
+		}
+	};
+
+	validator.extend(String.prototype, method);
 	
 	var Model = validator.Model = function (dom) {
 		if (!(dom && dom instanceof Element)) {
@@ -27,24 +59,6 @@
 			this._root = dom;
 		}
 	};
-	
-	validator.isPhone = function (str) {
-		return /^(?:(?:1(?:3[4-9]|5[012789]|8[78])\d{8}|1(?:3[0-2]|5[56]|8[56])\d{8}|18[0-9]\d{8}|1[35]3\d{8})|14[57]\d{8}|170[059]\d{7}|17[67]\d{8})$/.test(str);
-	};
-	validator.isMail = function (str) {
-		return /^([_a-zA-Z\d\-\.])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/.test(str);
-	};
-	validator.isFill ＝ function (str) {
-		return str.length !== 0;
-	}
-	validator.isPassword = function (str) {
-		return /^[a-zA-Z0-9_-]{6,20}$/.test(str);
-	}
-	String.prototype.isPhone = function() {
-		return /^(?:(?:1(?:3[4-9]|5[012789]|8[78])\d{8}|1(?:3[0-2]|5[56]|8[56])\d{8}|18[0-9]\d{8}|1[35]3\d{8})|14[57]\d{8}|170[059]\d{7}|17[67]\d{8})$/.test(this);
-	};
-	
-	
+
 	return validator;
 }));
-
