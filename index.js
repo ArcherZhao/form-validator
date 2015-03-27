bc5a8053fbee4d086d32b3d0b47e7e922fff2baf
ArcherZhao <zlming1015@gmail.com>
2015-03-27 23:12:16 +0800

    Add judgment process

--- index.js
+++ index.js
@@ -20,42 +20,120 @@
 		return this;
 	};
 
-	validator.extend = function (obj) {
-		var type = typeof obj;
-		if (!(type === 'function' || type === 'object' && !!obj)) return obj;
-		var source, prop;
-		for (var i = 1, length = arguments.length; i < length; i++) {
-			source = arguments[i];
-			for (prop in source) {
-				if (hasOwnProperty.call(source, prop)) {
-					obj[prop] = source[prop];
-				}
-			}
-		}
-		return obj;
-	};
 	var regPhone = /^(?:(?:1(?:3[4-9]|5[012789]|8[78])\d{8}|1(?:3[0-2]|5[56]|8[56])\d{8}|18[0-9]\d{8}|1[35]3\d{8})|14[57]\d{8}|170[059]\d{7}|17[67]\d{8})$/,
 		regMail = /^([_a-zA-Z\d\-\.])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/;
 
-	validator.isPhone = function (str){
+	validator.isPhone = function (str) {
 		return regPhone.test(str);
 	};
-	validator.isMail = function(str){
+	validator.isMail = function (str) {
 		return regMail.test(str);
-	}
+	};
 	validator.isFill = function (str) {
 		return str.length !== 0;
-	}
+	};
+	validator.isNumber = function (str) {
+		return !isNaN(Number(str));
+	};
 
 
-	
+
 	var Model = validator.Model = function (dom) {
-		if (!(dom && dom instanceof Element)) {
+		if (!dom) {
 			this._root = null;
 		} else {
 			this._root = dom;
 		}
+		this.valid = function (callback) {
+			this._root.find('input').each(function (i, ele) {
+				var $ele = $(ele);
+				var val = $ele.val();
+				if ($ele.attr("required") && val.length === 0) {
+
+					$ele.addClass("wrong").attr({
+						'rg-msg': "null",
+						'rg-status': 'wrong'
+					});
+
+				} else if ($ele.attr('maxlength') && val.length > $ele.attr('maxlength')) {
+
+					$ele.addClass("wrong").attr({
+						'rg-status': 'wrong',
+						'rg-msg': "tooLong"
+					});
+
+				} else if ($ele.attr('max') && Number(val) > $ele.attr('max')) {
+
+					$ele.addClass("wrong").attr({
+						'rg-status': 'wrong',
+						'rg-msg': "tooBig"
+					});
+
+				} else if ($ele.attr('min') && Number(val) < $ele.attr('min')) {
+
+					$ele.addClass("wrong").attr({
+						'rg-status': 'wrong',
+						'rg-msg': "tooSmall"
+					});
+
+				} else if ($ele.attr('rg-type')) {
+					switch ($ele.attr('rg-type')) {
+					case 'number':
+						if (!validator.isNumber(val)) {
+							$ele.addClass("wrong").attr({
+								'rg-status': 'wrong',
+								'rg-msg': "noNumber"
+							});
+						}
+						break;
+					case 'tel':
+						if (validator.isPhone(val)) {
+							$ele.addClass("wrong").attr({
+								'rg-status': 'wrong',
+								'rg-msg': "noPhone"
+							});
+						}
+						break;
+					case 'email':
+						if (validator.isMail(val)) {
+							$ele.addClass("wrong").attr({
+								'rg-status': 'wrong',
+								'rg-msg': "noMail"
+							});
+						}
+						break;
+					default:
+						break;
+					}
+				}
+			});
+			this._root.find('textarea').each(function (i, ele) {
+				var $ele = $(ele);
+				var val = $ele.val();
+				if ($ele.attr("required") && val.length === 0) {
+
+					$ele.addClass("wrong").attr({
+						'rg-status': 'wrong',
+						'rg-msg': "null"
+					});
+
+				} else if ($ele.attr('maxlength') && val.length > $ele.attr('maxlength')) {
+
+					$ele.addClass("wrong").attr({
+						'rg-status': 'wrong',
+						'rg-msg': "tooLong"
+					});
+
+				}
+			});
+			if(this._root.find('[rg-status="wrong"]').length > 0) {
+				callback(this._root.find('[rg-status="wrong"]'));
+			} else {
+				callback(null, this._root);
+			}
+		};
 	};
+
 
 	return validator;
 }));
